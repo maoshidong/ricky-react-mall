9305f30f6673ccb5bcdf451f48a3163921462286
salamexperts <salamexperts@gmail.com>
2024-11-26 16:21:27 +0500

    Added the open graph tags in quality page

--- pages/quality/index.jsx
+++ pages/quality/index.jsx
@@ -23,7 +23,8 @@ const QualityPage = ({ paramMap }) => {
 				<meta property="og:title" content={i18Title} key="og:title" />
 				<meta name="keywords" content={i18Key} key="keywords" />
 				<meta name="description" content={i18Des} key="description" />
-				<meta name="og:description" content={i18Des} key="og:description" />
+				<meta property="og:description" content={i18Des} key="og:description" />
+				<meta property="og:image" content="https://www.origin-ic.com/static/img/logo.png" key="og:image" />
 			</Head>
 			<main id="homepage-zqx">
 				{/* ps-product--detail */}
